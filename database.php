7b608f426f111cc200d0e011907540a42210944a
Michelle Regna <michelleregna@gmail.com>
2017-11-14 16:54:53 -0500

    first commit

---
+++ database.php
@@ -0,0 +1,53 @@
+
+<?php
+
+class dbConn{
+	// $dsn = 'mysql:host=sql1.njit.edu;dbname=mcr35';
+	// $username = 'mcr35';
+	// $pw = 'diehard53';
+    //variable to hold connection object.
+    protected static $db;
+    //private construct - class cannot be instatiated externally.
+    private function __construct() {
+        try {
+            // assign PDO object to db variable
+            self::$db = new PDO('mysql:host=sql1.njit.edu;dbname=mcr35', 'mcr35', 'diehard53');
+            self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        }
+        catch (PDOException $e) {
+            //Output error - would normally log this to error file rather than output to user.
+            echo "Connection Error: " . $e->getMessage();
+        }
+    }
+    // get connection function. Static method - accessible without instantiation
+    public static function getConnection() {
+        //Guarantees single instance, if no connection object exists then create one.
+        if (!self::$db) {
+            //new connection object.
+            new dbConn();
+        }
+        //return connection.
+        return self::$db;
+    }
+}
+
+
+
+
+
+
+// ini_set('display_errors', 'On');
+
+// $dsn = 'mysql:host=sql1.njit.edu;dbname=mcr35';
+// $username = 'mcr35';
+// $pw = 'diehard53';
+
+// try {
+// 	$db =new PDO($dsn, $username, $pw);
+// 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+// 	echo 'Connection failed: ' . $e->getMessage() . '</br>';
+
+// }
+
+?>
---
+++ index.php
@@ -0,0 +1,55 @@
+
+<?php 
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+include('database.php');
+include('model.php');
+include('account.php');
+include('todo.php');
+include('collection.php');
+include('accounts.php');
+include('todos.php');
+
+
+// $dsn = 'mysql:host=sql1.njit.edu; dbname=mcr35';
+// $username = 'mcr35';
+// $pw = 'diehard53';
+
+// $dbo = dbConn::getConnection();
+$records = accounts::findAll();
+print_r($records);
+
+// $account = new account();
+// $account->load('1');
+
+// echo $account;
+
+// echo 'hello';
+
+
+
+// this would be the method to put in the index page for accounts
+// $records = accounts::findAll();
+//print_r($records);
+// this would be the method to put in the index page for todos
+// $records = todos::findAll();
+//print_r($records);
+//this code is used to get one record and is used for showing one record or updating one record
+// $record = todos::findOne(1);
+//print_r($record);
+//this is used to save the record or update it (if you kndow how to make update work and insert)
+// $record->save();
+//$record = accounts::findOne(1);
+//This is how you would save a new todo item
+// $record = new todo();
+// $record->message = 'some task';
+// $record->isdone = 0;
+//$record->save();
+// print_r($record);
+// $record = todos::create();
+// print_r($record);
+
+
+?>
