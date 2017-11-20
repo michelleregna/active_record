ab7898c858ce95b16a29d4bc22b751b692b19551
Michelle Regna <michelleregna@gmail.com>
2017-11-20 13:57:48 -0500

    save todo function

--- model.php
+++ model.php
@@ -30,6 +30,32 @@ public function save_account($id = null, $email, $fname, $lname, $phone, $birthd
 
 }
 
+public function save_todo($id = null, $owneremail, $ownerid, $createddate, $duedate, $message, $isdone) {
+
+    if ($id) {
+        $sql = 'UPDATE todos SET owneremail = ?, ownerid = ?, createddate = ?, duedate = ?, message = ?, isdone = ? WHERE id = ?';
+    } else {
+        $sql = 'INSERT INTO todos(owneremail, ownerid, createddate, duedate, message, isdone) VALUES(?, ?, ?, ?, ?, ?)';
+    }
+
+    $db = database::getConnection();
+    $results = $db->prepare($sql);
+    $results->bindValue(1, $owneremail, PDO::PARAM_STR);
+    $results->bindValue(2, $ownerid, PDO::PARAM_INT);
+    $results->bindValue(3, $createddate, PDO::PARAM_STR);
+    $results->bindValue(4, $duedate, PDO::PARAM_STR);
+    $results->bindValue(5, $message, PDO::PARAM_STR);
+    $results->bindValue(6, $isdone, PDO::PARAM_INT);
+
+    if ($id) {
+            $results->bindValue(7, $id, PDO::PARAM_INT);
+        }
+
+    $results->execute();
+
+
+}
+
 public function delete($tableName, $id) {
         
         $db = database::getConnection();
--- start.php
+++ start.php
@@ -2,7 +2,7 @@
 <?php
 class start extends page {
 	public function get() {
-				// select all
+		// select all
 		$this->html .= '<h2>Select All Accounts</h2>';
 		$records = accounts::findAll();
 		$this->html .= htmlTable::buildTable($records);
@@ -19,7 +19,7 @@ class start extends page {
 		$this->html .= htmlTable::buildTable($records); 
 
 		// insert a record
-		$this->html .= '<h2>Insert Record</h2>';
+		$this->html .= '<h2>Insert Account Record</h2>';
 		$record = new account();
 		$record->save_account('','finn@aol.com', 'Finnegan', 'Regna', '7324078497', '10242010', 'male', 'woof');
 		$records = accounts::findAll();
@@ -46,6 +46,20 @@ class start extends page {
 		$record->delete('todos', 5);
 		$records = todos::findAll();
 		$this->html .= htmlTable::buildTable($records); 
+
+		//insert a todo
+		$this->html .= '<h2>Insert Todo</h2>';
+		$record = new todo();
+		$record->save_todo('','finn@aol.com', 5, '112017', '111917', 'this is killing me', 0);
+		$records = todos::findAll();
+		$this->html .= htmlTable::buildTable($records);
+
+		//update to do
+		$this->html .= '<h2>Update Todo Record 7</h2>';
+		$record->save_todo('7','mcr35@njit.edu', 10, 'sorry', '12400', 'this is bad', 0);
+		$records = todos::findAll();
+		$this->html .= htmlTable::buildTable($records);
+
 
 		
 	}
